eac5d7d1b3b7c227e9e022205ff9e9e8f0053efd
root <yuribreion@gmail.com>
2019-09-04 15:18:05 -0300

    working with binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html' 
+    templateUrl: './server.component.html'
 
 })
 
 export class ServerComponent {
+    serverId = 10;
+    serverStatus = 'Offline';
 
+    getServerStatus() {
+        return this.serverStatus;
+    }
 }
